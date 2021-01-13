eec4f75cafddbbb6966576e6d0b841cf86d74bfb
Patrick Narciso <patrick.ncsilva@gmail.com>
2021-01-13 15:14:53 -0300

    feat(screens): creates home page with mocked data

--- src/screens/NotAuthenticated/Home/index.tsx
+++ src/screens/NotAuthenticated/Home/index.tsx
@@ -1,20 +1,101 @@
 import React from 'react';
+import { useWindowDimensions } from 'react-native';
 
-import { Container } from './styles';
+import BannerDay from '~/assets/images/svg/day-banner.svg';
+import PinIcon from '~/assets/icons/svg/pin.svg';
+import HumidityIcon from '~/assets/icons/svg/humidity.svg';
+import PressureIcon from '~/assets/icons/svg/pressure.svg';
+import WindIcon from '~/assets/icons/svg/wind.svg';
+import SunriseIcon from '~/assets/icons/svg/sunrise.svg';
+import SunsetIcon from '~/assets/icons/svg/sunset.svg';
+import DayTimeIcon from '~/assets/icons/svg/daytime.svg';
+import ArrowUp from '~/assets/icons/svg/arrow-up.svg';
+import ArrowDown from '~/assets/icons/svg/arrow-down.svg';
 
-import { Card } from '~/components';
+import { Container, Panel } from './styles';
 
-const Signin: React.FC = () => {
+const Home: React.FC = () => {
+  const screenWidth = useWindowDimensions().width;
+
+  const cards = [
+    {
+      title: '49%',
+      subtitle: 'Humidity',
+      icon: <HumidityIcon />
+    },
+    {
+      title: '1,007mBar',
+      subtitle: 'Pressure',
+      icon: <PressureIcon />
+    },
+    {
+      title: '23 km/h',
+      subtitle: 'Wind',
+      icon: <WindIcon />
+    },
+    {
+      title: '6:03 AM',
+      subtitle: 'Sunrise',
+      icon: <SunriseIcon />
+    },
+    {
+      title: '7:05 PM',
+      subtitle: 'Sunset',
+      icon: <SunsetIcon />
+    },
+    {
+      title: '13h 1m',
+      subtitle: 'Daytime',
+      icon: <DayTimeIcon />
+    }
+  ];
+
   return (
     <Container>
-      <Card
-        iconPath="https://openweathermap.org/img/w/02d.png"
-        title="Mon, 21"
-        highTemp={30}
-        lowTemp={10}
-      />
+      <BannerDay width={screenWidth} />
+      <Panel.Container>
+        <Panel.Header>
+          <Panel.HeaderText>Sunday, 19 may 2019 | 4:30PM</Panel.HeaderText>
+          <Panel.Geolocation>
+            Mumbai, India <PinIcon />
+          </Panel.Geolocation>
+        </Panel.Header>
+        <Panel.Main>
+          <Panel.Row>
+            <Panel.Card>
+              <Panel.WeatherIcon
+                source={{ uri: 'https://openweathermap.org/img/wn/10d@2x.png' }}
+              />
+              <Panel.WeatherText>Sunny</Panel.WeatherText>
+            </Panel.Card>
+            <Panel.Card flexDirection="row">
+              <Panel.Temperature>33</Panel.Temperature>
+              <Panel.Measure>°C</Panel.Measure>
+            </Panel.Card>
+            <Panel.Card>
+              <Panel.CurrentTemp>
+                35°C
+                <ArrowUp />
+              </Panel.CurrentTemp>
+              <Panel.CurrentTemp>
+                27°C
+                <ArrowDown />
+              </Panel.CurrentTemp>
+            </Panel.Card>
+          </Panel.Row>
+          <Panel.Row>
+            {cards.map((card) => (
+              <Panel.Card>
+                {card.icon}
+                <Panel.CartTitle>{card.title}</Panel.CartTitle>
+                <Panel.CardSubtitle>{card.subtitle}</Panel.CardSubtitle>
+              </Panel.Card>
+            ))}
+          </Panel.Row>
+        </Panel.Main>
+      </Panel.Container>
     </Container>
   );
 };
 
-export default Signin;
+export default Home;
--- src/screens/NotAuthenticated/Home/styles.ts
+++ src/screens/NotAuthenticated/Home/styles.ts
@@ -1,8 +1,95 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.SafeAreaView`
+export const Container = styled.View`
   flex: 1;
   background-color: ${({ theme }) => theme.colors.background};
-  justify-content: center;
-  align-items: center;
 `;
+
+export const Panel = {
+  Container: styled.View`
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    top: 280px;
+    background-color: #ffffff;
+    border-top-left-radius: 30px;
+    border-top-right-radius: 30px;
+  `,
+  Header: styled.View`
+    flex-direction: row;
+    justify-content: space-between;
+    padding: 20px;
+  `,
+  HeaderText: styled.Text`
+    font-family: ${({ theme }) => theme.fonts.regular};
+    font-size: 14px;
+    color: ${({ theme }) => theme.colors.lightText};
+  `,
+  Geolocation: styled.Text`
+    font-family: ${({ theme }) => theme.fonts.regular};
+    font-weight: 500;
+    font-size: 16px;
+    line-height: 19px;
+    color: ${({ theme }) => theme.colors.primary};
+  `,
+  Main: styled.View`
+    padding: 20px;
+  `,
+  Row: styled.View`
+    flex-direction: row;
+    justify-content: space-between;
+    flex-wrap: wrap;
+  `,
+  Card: styled.View<{ flexDirection?: string }>`
+    flex-direction: ${({ flexDirection }) => flexDirection || 'column'}
+    align-items: ${({ flexDirection }) =>
+      flexDirection ? 'flex-start' : 'center'};
+    justify-content: ${({ flexDirection }) =>
+      flexDirection ? 'flex-end' : 'center'}
+    flex-basis: 30%;
+    margin-bottom: 40px;
+  `,
+  CartTitle: styled.Text`
+    font-family: ${({ theme }) => theme.fonts.medium};
+    color: ${({ theme }) => theme.colors.text};
+    font-size: 16px;
+    line-height: 19px;
+    margin-top: 12px;
+    margin-bottom: 4px;
+  `,
+  CardSubtitle: styled.Text`
+    font-family: ${({ theme }) => theme.fonts.medium};
+    color: ${({ theme }) => theme.colors.lightText};
+    font-size: 8px;
+    line-height: 10px;
+  `,
+  WeatherIcon: styled.Image`
+    width: 50px;
+    height: 50px;
+  `,
+  WeatherText: styled.Text`
+    font-family: ${({ theme }) => theme.fonts.medium};
+    color: #000000;
+    font-size: 18px;
+    line-height: 22px;
+    margin-top: 4px;
+  `,
+  Temperature: styled.Text`
+    font-family: ${({ theme }) => theme.fonts.light};
+    color: #000000;
+    font-size: 64px;
+  `,
+  Measure: styled.Text`
+    font-family: ${({ theme }) => theme.fonts.medium};
+    color: #666666;
+    font-size: 24px;
+    margin-top: 15px;
+  `,
+  CurrentTemp: styled.Text`
+    font-family: ${({ theme }) => theme.fonts.light};
+    color: #666666;
+    font-size: 16px;
+    line-height: 19px;
+    margin-top: 12px;
+  `
+};

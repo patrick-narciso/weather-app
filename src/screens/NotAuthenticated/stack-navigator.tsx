f2f1dbcf8419cafc318238bb353e6a301c4490a9
Patrick Narciso <patrick.ncsilva@gmail.com>
2021-01-14 17:51:32 -0300

    feat(utils): creates utils for check connectivity

--- src/screens/NotAuthenticated/stack-navigator.tsx
+++ src/screens/NotAuthenticated/stack-navigator.tsx
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
+
+import NetworkUtils from '~/utils/network-utils';
 import { RootStackParamList } from '~/root-navigator';
 
 import Home from './Home';
+import Error from './Error';
 
 const Stack = createStackNavigator<RootStackParamList>();
 
 const NotAuthenticatedStackNavigator: React.FC = () => {
+  const [initialRoute, setInitialRoute] = useState<'Home' | 'Error'>('Home');
+
+  useEffect(() => {
+    (async function () {
+      const isConnected = await NetworkUtils.isNetworkAvailable();
+      setInitialRoute(isConnected ? 'Home' : 'Error');
+    })();
+  }, []);
+
   return (
-    <Stack.Navigator initialRouteName="Home" headerMode="none">
+    <Stack.Navigator initialRouteName={initialRoute} headerMode="none">
       <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen name="Error" component={Error} />
     </Stack.Navigator>
   );
 };
